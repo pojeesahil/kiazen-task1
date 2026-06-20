740e6b2f296d3f9c8ffe8e8a0ac7e28e06f205cc
pojeesahil <pojeesahil@gmail.com>
2026-06-20 19:35:58 +0530

    added UnstructuredFileLoader to load all txt and pdf files

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from langchain.agents import create_agent
 from dotenv import load_dotenv
 import os
@@ -5,6 +6,7 @@ from langchain.chat_models import init_chat_model
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_chroma import Chroma
+from langchain_community.document_loaders import UnstructuredFileLoader
 load_dotenv("secure.env")
 api_key = os.getenv("GOOGLE_API_KEY") or os.getenv("GEMINI_API_KEY")
 llm = init_chat_model(
@@ -25,16 +27,22 @@ result = agent.invoke(
     {"messages": [{"role": "user", "content": "What's the weather in San Francisco?"}]}
 )
 print(result["messages"][-1].content_blocks)
-with open("test.txt") as f:
-    txt=f.read()
-
+mainDir=Path("./data")
+files=[]
+for file in mainDir.iterdir():
+    if file.suffix==".txt" or file.suffix==".pdf":
+        files.append(str(file))
+txt=[]
+for path in files:
+    loader=UnstructuredFileLoader(file_path=path)
+    txt.extend(loader.load())
 text_splitter=RecursiveCharacterTextSplitter(
     chunk_size=100,
     chunk_overlap=20,
     length_function=len,
     is_separator_regex=False,
 )
-texts=text_splitter.create_documents([txt])
+texts=text_splitter.split_documents(txt)
 embeddings=GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
 
 vectorstore=Chroma.from_documents(
